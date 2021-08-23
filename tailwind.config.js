1b53b5c1126c95e539f7d7c055e4382b52bc2fb6
James Tarpey <hello@jamestarpey.com>
2021-08-23 14:20:58 +0100

    Configure Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+    },
+  },
+  variants: {
+    extend: {
+      opacity: ["disabled"],
+    },
+  },
+  plugins: [],
+};
